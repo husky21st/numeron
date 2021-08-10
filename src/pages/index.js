bde60d7d85684993ed793e80650ff6f4e2d951de
Husky21st <husky21.mn@gmail.com>
2021-08-11 00:10:19 +0900

    fix input area

--- src/pages/index.js
+++ src/pages/index.js
@@ -35,10 +35,21 @@ function History(){
 	//const [numbers, setNumbers] = useState(null);
 }
 
-function Clear(){
+function Clear(props){
+	function handleRestart(event){
+		event.preventDefault();
+		props.restart(setnumber);
+		props.submit([]);
+		props.check(0);
+	}
 	return(
 		<div className='h-screen w-screen bg-gray-100 bg-opacity-80 fade'>
-			<p className='h-screen text-red-500 text-8xl flex justify-center items-center'>Clear!</p>
+			<div className='flex flex-col h-screen justify-center items-center'>
+				<p className=' text-red-500 text-8xl'>Clear!</p>
+				<div className='p-8'>
+					<button onClick={handleRestart} className="bg-blue-500 hover:bg-blue-700 text-white font-bold text-xl py-3 px-6 rounded-full">Restart</button>
+				</div>
+			</div>
 		</div>
 	);
 }
@@ -87,10 +98,17 @@ function CheckNumber(props){
 }
 
 function InputArea(props){
-	
 	//console.log(props.submitNumber);
+	const [init, change] = useState(false);
+	useEffect(() => {
+		change(true);
+	},[props.submitNumber]);
+	function choice() {
+		change(false);
+	}
 	function handleSubmit(event){
 		event.preventDefault();
+		change(true);
 		const number = event.target.elements;
 		if(number.first.value === number.second.value ||
 			number.first.value === number.third.value ||
@@ -103,46 +121,39 @@ function InputArea(props){
 		//console.log(props.Answer);
 		//setNumbers(submitNumber);
 	}
+	const options = [0,1,2,3,4,5,6,7,8,9];
+	console.log(init);
 	return(
 		<div>
 			<form className='flex flex-col items-center align-center' onSubmit={handleSubmit}>
 				<div className='ring rounded md:text-5xl text-3xl '>
-					<select name='first' defaultValue='0'>
-						<option value='0'>0</option>
-						<option value='1'>1</option>
-						<option value='2'>2</option>
-						<option value='3'>3</option>
-						<option value='4'>4</option>
-						<option value='5'>5</option>
-						<option value='6'>6</option>
-						<option value='7'>7</option>
-						<option value='8'>8</option>
-						<option value='9'>9</option>
-					</select>
-					<select name='second' defaultValue='0'>
-						<option value='0'>0</option>
-						<option value='1'>1</option>
-						<option value='2'>2</option>
-						<option value='3'>3</option>
-						<option value='4'>4</option>
-						<option value='5'>5</option>
-						<option value='6'>6</option>
-						<option value='7'>7</option>
-						<option value='8'>8</option>
-						<option value='9'>9</option>
-					</select>
-					<select name='third' defaultValue='0'>
-						<option value='0'>0</option>
-						<option value='1'>1</option>
-						<option value='2'>2</option>
-						<option value='3'>3</option>
-						<option value='4'>4</option>
-						<option value='5'>5</option>
-						<option value='6'>6</option>
-						<option value='7'>7</option>
-						<option value='8'>8</option>
-						<option value='9'>9</option>
-					</select>
+				{
+					init?(
+						<div>
+						<select name='first' value='0' onChange={choice}>
+						{options.map((o) => <option key={o} value={o}>{o}</option>)}
+						</select>
+						<select name='second' value='0' onChange={choice}>
+						{options.map((o) => <option key={o} value={o}>{o}</option>)}
+						</select>
+						<select name='third' value='0' onChange={choice}>
+						{options.map((o) => <option key={o} value={o}>{o}</option>)}
+						</select>
+						</div>
+					) : (
+						<div>
+						<select name='first'>
+						{options.map((o) => <option key={o} value={o}>{o}</option>)}
+						</select>
+						<select name='second'>
+						{options.map((o) => <option key={o} value={o}>{o}</option>)}
+						</select>
+						<select name='third'>
+						{options.map((o) => <option key={o} value={o}>{o}</option>)}
+						</select>
+						</div>
+					)
+				}
 				</div>
 				<div className='p-3'>
 					<button type='submit' className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
@@ -157,7 +168,7 @@ function InputArea(props){
 export default function Home() {
 	const [submitCount, check] = useState(0);
 	const [submitNumber, submit] = useState([]);
-	const [AnswerNumber] = useState(setnumber);
+	const [AnswerNumber, restart] = useState(setnumber);
 	console.log(AnswerNumber);
   return (
     <div className='h-screen flex flex-col'>
@@ -168,14 +179,14 @@ export default function Home() {
       <main className="flex flex-col flex-1 text-center font-reggae ">
 		  <div className='pt-5'>Input Your Answer</div>
 		  <div className='pt-1'><InputArea submit={submit} submitNumber={submitNumber}
-		  check={check} submitCount={submitCount} /></div>
+		  check={check} submitCount={submitCount}/></div>
 		  {
 				submitCount > 0 &&
 				<div><CheckNumber submit={submitNumber} answer={AnswerNumber} count={submitCount} /></div>
 			}
 			{
 				submitCount > 0 && submitNumber?.every((x,i) => parseInt(x,10) === AnswerNumber[i] ) &&
-				<div className='absolute z-50 ' ><Clear /></div>
+				<div className='absolute z-50 ' ><Clear check={check} submit={submit} restart={restart} /></div>
 			}
 	  </main>
 	  <footer className="w-full flex justify-center items-center h-12 border-t">
